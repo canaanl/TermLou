f7d35bfc72f6da9477056deaf9890900d3216e01
Lou <lou@termlou.local>
2026-09-19 10:24:19 +0800

    5.0.43: boxed inscribed launcher trio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.workspace.proot"
         minSdk = 26
         targetSdk = 34
-        versionCode = 542
-        versionName = "5.0.42"
+        versionCode = 543
+        versionName = "5.0.43"
         ndk { abiFilters.add("arm64-v8a") }
     }
 
